e585f39674e06a39ba38758c5d754c44d69c7f19
Shecspi <shecspi@yandex.ru>
2024-10-09 13:15:07 +0300

    Добавлен полигон Башкирии

--- src/hello.py
+++ src/hello.py
@@ -1,105 +1,24 @@
-import json
-import time
-
-import aiofiles
-import redis
-from pathlib import Path
-
-from fastapi import FastAPI, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
-
-polygons_dir = '/home/shecspi/Projects/geo-polygons'
-
-
-app = FastAPI()
-router_country = APIRouter()
-router_region = APIRouter()
-router_city = APIRouter()
-
-r = redis.StrictRedis(
-    host='localhost',
-    port=6379,
-    password='password'
-)
-
-origins = [
-    '*'
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-async def root():
-    path = Path(f'/home/shecspi/Projects/geo-polygons/regions/RU')
-    files = path.glob('*.geojson')
-    result = []
-    for file in files:
-        with open(file, 'r') as f:
-            result.append(json.load(f))
-
-    return result
-
-
-@router_country.get('/all')
-async def get_all_countries():
-    """
-    Возвращает полигоны всех стран мира.
-    """
-
-
-@router_country.get('/{country_code}')
-async def get_country(country_code: str):
-    """
-    Возвращает полигон переданной страны.
-    """
-
-
-@router_region.get('/{country_code}/all')
-async def get_all_regions(country_code: str):
-    """
-    Возвращает полигоны всех регионов указанной страны.
-    """
-    p = Path(polygons_dir) / f'regions/{country_code}'
-    files = p.glob('*.geojson')
-    result = []
-
-    start_time = time.perf_counter()
-    for file in files:
-        async with aiofiles.open(file, 'r') as f:
-            a = await f.read()
-    print(time.perf_counter() - start_time)
-
-    return result
-
-
-@router_region.get('/{country_code}/{region_code}')
-async def get_region(country_code: str, region_code: str):
-    """
-    Возвращает полигон указанного региона из указанной страны.
-    """
-
-
-@router_region.get('/{country_code}/{area_code}/{region_code}')
-async def get_regions_in_area(country_code: str, area_code: str, region_code: str):
-    """
-    Возвращает полигоны всех регионов в указанном округе указанной страны.
-    Это может быть доступно не для всех стран, так как не у всех можеь быть деление на округа.
-    """
-
-
-@router_city.get('/{country_code}/all')
-async def get_all_cities(country_code: str, region_code: str):
-    """
-    Возвращает geoJson-точки всех городов указанной страны.
-    """
-
-
-app.include_router(router_country, prefix='/country', tags=['country'])
-app.include_router(router_region, prefix='/region', tags=['region'])
-app.include_router(router_city, prefix='/city', tags=['city'])
+{
+    'order_uid': '77b995be-6f6e-11ef-81a5-089e01ea98d8',
+    'order_action': 'VACATION',
+    'order_num': '374Л',
+    'order_date': '2024-10-01T15:16:12',
+    'order_passed': True,
+    'order_deleted': False,
+    'org_uid': 'e5c8ee49-a508-11e7-8107-089e01ea98d9',
+    'org_fullname': 'Акционерное общество  МОСИТАЛМЕД',
+    'org_shortname': 'Акционерное общество  МОСИТАЛМЕД',
+    'fl_code': 'ЗП-0000373',
+    'date_birth': '1957-11-26T00:00:00',
+    'gender': 'Мужской',
+    'last_name': 'Ермаков',
+    'first_name': 'Олег',
+    'middle_name': 'Валентинович',
+    'staff_timesheet_number': '00ЗП-00524',
+    'department_name': None,
+    'position_name': None,
+    'date_from': '2024-10-07T00:00:00',
+    'date_to': '2024-10-13T00:00:00',
+    'is_base_info_change': None,
+    'mod': 'ОТ'
+}
